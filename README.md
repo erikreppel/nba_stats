92a7f7609aaf595fa7603b6e43b1225803f101de
Erik Reppel <erikreppel@gmail.com>
2016-03-23 21:33:09 -0700

    fixed the player functionality and can now return a data frame broken down by season

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-In progress...
+nba_stats
+===
+
+A utility for accessing data from the NBA stats website. 
+
+Note: all data is property of the NBA, this is merely a utility to make programmatic access of the data easier.
+
+## Install: 
+
+Coming soon.. If you want to try it out, clone the repo and include nba_stats.py
--- nba_stats.py
+++ nba_stats.py
@@ -1,134 +1,59 @@
 import requests
+import json
+import pandas as pd
+
+HEADERS = {'User-Agent': 'Mozilla/5.0'}
 
 
-league_leaders_url = 'http://stats.nba.com/stats/leagueleaders?LeagueID=00&PerMode=PerGame&Scope=S&Season=2015-16&SeasonType=Pre+Season&StatCategory=REB'
-league_leaders = requests.get(league_leaders_url).json()
-player_ids = {i[2]: i[0] for i in league_leaders['resultSet']['rowSet']}
+class Player:
+
+    def __init__(self, player_name, stat_type='PerGame', breakdown='career'):
+        self.player_name = player_name
+        league_leaders_url = 'http://stats.nba.com/stats/leagueleaders?LeagueID=00&PerMode=PerGame&Scope=S&Season=2015-16&SeasonType=Pre+Season&StatCategory=REB'
+        league_leaders = requests.get(
+            league_leaders_url, headers=HEADERS).json()
+        self.player_ids = {i[2]: i[0]
+                           for i in league_leaders['resultSet']['rowSet']}
+
+        self.stats = {}
+        self.get_career_stats(player_name, stat_type)
+
+    def df(self, season_type='SeasonTotalsRegularSeason'):
+        s = self.stats[season_type]
+        return pd.DataFrame(s['seasons'], columns=s['headers'])
+
+    def get_career_stats(self, player_name, stat_type='PerGame'):
+        career_url = '''http://stats.nba.com/stats/playercareerstats?LeagueID=00&PerMode=%(stat_type)s&PlayerID=%(player_id)s''' % \
+            {
+                "stat_type": stat_type,
+                "player_id": self.player_ids[player_name]
+            }
+        resp = requests.get(career_url, headers=HEADERS)
+        # print resp
+        if resp.status_code != 200:
+            raise Exception(resp.text)
+        else:
+            data = resp.json()
+            headers = data['resultSets'][0]['headers']
+            for i in data['resultSets']:
+                print i['name']
+
+                parsed = []
+                for row in range(len(i['rowSet'])):
+                    season = {}
+                    for j in range(len(i['rowSet'][row])):
+                        season[headers[j]] = i['rowSet'][row][j]
+                    parsed.append(season)
+                self.stats[i['name']] = {
+                    'headers': headers,
+                    'seasons': parsed
+                }
 
 
-def get_team_stats(conference='',
-                   date_from='',
-                   date_to='',
-                   division='',
-                   game_scope='',
-                   game_segment='',
-                   last_n_games='0',
-                   league_id='00',
-                   location='',
-                   measure_type='Base',
-                   month='0',
-                   opponent_team_id='0',
-                   outcome='',
-                   po_round='0',
-                   pace_adjust='N',
-                   per_mode='PerGame',
-                   period='0',
-                   player_experience='',
-                   player_position='',
-                   plus_minus='N',
-                   rank='N',
-                   season='2015-16',
-                   season_segment='',
-                   season_type='Regular+Season',
-                   shot_clock_range='',
-                   started_bench='',
-                   team_id='0',
-                   vs_conference='',
-                   vs_division=''):
-    stats_url = 'http://stats.nba.com/stats/leaguedashteamstats?Conference=%s&DateFrom=%s&DateTo=%s&Division=%s&GameScope=%s&GameSegment=%s&LastNGames=%s&LeagueID=%s&Location=%s&MeasureType=%s&Month=%s&OpponentTeamID=%s&Outcome=%s&PORound=%s&PaceAdjust=%s&PerMode=%s&Period=%s&PlayerExperience=%s&PlayerPosition=%s&PlusMinus=%s&Rank=%s&Season=%s&SeasonSegment=%s&SeasonType=%s&ShotClockRange=%s&StarterBench=&%sTeamID=%s&VsConference=%s&VsDivision=%s' % (
-        conference,
-        date_from,
-        date_to,
-        division,
-        game_scope,
-        game_segment,
-        last_n_games,
-        league_id,
-        location,
-        measure_type,
-        month,
-        opponent_team_id,
-        outcome,
-        po_round,
-        pace_adjust,
-        per_mode,
-        period,
-        player_experience,
-        player_position,
-        plus_minus,
-        rank,
-        season,
-        season_segment,
-        season_type,
-        shot_clock_range,
-        started_bench,
-        team_id,
-        vs_conference,
-        vs_division)
-    resp = requests.get(stats_url)
-    if resp.status_code == 200:
-        return resp.json()
-    else:
-        raise Exception(resp.text)
+if __name__ == "__main__":
+    lebron = Player("LeBron James", "PerGame")
+    # print lebron.stats['SeasonTotalsRegularSeason']
 
-
-team_stats_2014 = get_team_stats(season='2014-15')
-team_ids = {x[1]: x[0] for x in team_stats_2014['resultSets'][0]['rowSet']}
-
-
-def get_player_stats(player=None,
-                     date_from='',
-                     date_to='',
-                     game_segment='',
-                     last_n_games='0',
-                     league_id='00',
-                     location='',
-                     measure_type='Base',
-                     month='0',
-                     opponent_team_id='0',
-                     outcome='',
-                     po_round='',
-                     pace_adjust='N',
-                     per_mode='PerGame',
-                     period='0',
-                     player_id='203080',
-                     plus_minus='N',
-                     rank='N',
-                     season='2015-16',
-                     season_segment='',
-                     season_type='Regular+Season',
-                     ShotClockRange='',
-                     vs_conference='',
-                     vs_division=''
-                     ):
-    if player:
-            player_id = player_ids[player]
-    stats_url = 'http://stats.nba.com/stats/playerdashboardbygeneralsplits?DateFrom=%s&DateTo=%s&GameSegment=%s&LastNGames=%s&LeagueID=%s&Location=%s&MeasureType=%s&Month=%s&OpponentTeamID=%s&Outcome=%s&PORound=%s&PaceAdjust=%s&PerMode=%s&Period=%s&PlayerID=%s&PlusMinus=%s&Rank=%s&Season=%s&SeasonSegment=%s&SeasonType=%s&ShotClockRange=%s&VsConference=%s&VsDivision=%s' % (
-      date_from,
-      date_to,
-      game_segment,
-      last_n_games,
-      league_id,
-      location,
-      measure_type,
-      month,
-      opponent_team_id,
-      outcome,
-      po_round,
-      pace_adjust,
-      per_mode,
-      period,
-      player_id,
-      plus_minus,
-      rank,
-      season,
-      season_segment,
-      season_type,
-      ShotClockRange,
-      vs_conference,
-      vs_division)
-    resp = requests.get(stats_url)
-    if resp.status_code == 200:
-        return resp.json()
-    else:
-        raise Exception(resp.text)
+    s = lebron.stats['SeasonTotalsRegularSeason']
+    print pd.DataFrame(s['seasons'], columns=s['headers'])
+    print s['seasons']
